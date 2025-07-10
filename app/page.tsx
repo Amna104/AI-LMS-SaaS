381e9e251d690767b39f6a4cb9655ff0d7353835
Amna104 <amnaarshad019@gmail.com>
2025-07-10 23:43:38 +0500

    implement responsive homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,50 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import React from 'react'
+import './globals.css'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Page</h1>
-    <Button>Lets get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+        id="123"
+        name="Neura the Brainy explorer"
+        topic="Neural Network of the brain"
+        subject="science"
+        duration={45}
+        color="#ffda6e"
+        />
+        <CompanionCard
+        id="456"
+        name="Neura the Brainy explorer"
+        topic="Neural Network of the brain"
+        subject="science"
+        duration={45}
+        color="#e5d0ff"
+        />
+        <CompanionCard
+        id="789"
+        name="Neura the Brainy explorer"
+        topic="Neural Network of the brain"
+        subject="science"
+        duration={45}
+        color="#BDE7FF"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList
+        title="Recently completed sessions"
+        companions={recentSessions}
+        classNames = "w-2/3 max-lg:w-full"
+        />
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
